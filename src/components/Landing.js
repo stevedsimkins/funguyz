8a57b03f562bfa1e57a18c6c3a44b52d46cd4470
Steve <stevedsimkins@gmail.com>
2022-02-05 20:15:37 -0500

    added different structure to fix spacing in hero images/text

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,21 +1,29 @@
-import styled from "styled-components";
-import logo from "../assets/logo.png";
-import landingHero from "../assets/landingHero.gif";
-import twitter from "../assets/twitter2.svg";
-import discord from "../assets/discord2.svg";
+import styled from 'styled-components'
+import logo from '../assets/logo.png'
+import landingHero from '../assets/landingHero.gif'
+import twitter from '../assets/twitter2.svg'
+import discord from '../assets/discord2.svg'
 
 const Landing = () => {
   return (
-    <LandingSection className="gradient1">
+    <LandingSection className='gradient1'>
       <LandingNav>
-        <TwitterButton href="https://twitter.com/FunguyzNFT" target="_blank"><img src={twitter} alt="twitter Icon"/></TwitterButton>
-        <DiscordButton href="https://discord.gg/d7NxUfYwDy" target="_blank"><img src={discord} alt="discord icon"/></DiscordButton>
+        <TwitterButton href='https://twitter.com/FunguyzNFT' target='_blank'>
+          <img src={twitter} alt='twitter Icon' />
+        </TwitterButton>
+        <DiscordButton href='https://discord.gg/d7NxUfYwDy' target='_blank'>
+          <img src={discord} alt='discord icon' />
+        </DiscordButton>
       </LandingNav>
-     <Logo src={logo} alt="logo" /> 
-     <LandingHero src={landingHero} alt="funguyz hero" />
-     <Copy>3,000 Happy-go-lucky Mushroom NFTs Born on the Solana Blockchain</Copy>
-     <MintButton>
-       <h4 className="gradientText">Coming Soon!</h4>
+      <HeroContainer>
+        <Logo src={logo} alt='logo' />
+        <LandingHero src={landingHero} alt='funguyz hero' />
+        <Copy>
+          3,000 Happy-go-lucky Mushroom NFTs Born on the Solana Blockchain
+        </Copy>
+      </HeroContainer>
+      <MintButton>
+        <h4 className='gradientText'>Coming Soon!</h4>
       </MintButton>
     </LandingSection>
   )
@@ -28,7 +36,7 @@ const LandingSection = styled.div`
   justify-content: space-between;
   align-items: center;
   flex-direction: column;
-`;
+`
 
 const LandingNav = styled.div`
   width: 100%;
@@ -38,17 +46,17 @@ const LandingNav = styled.div`
   align-items: center;
   margin-right: 25%;
   gap: 4rem;
-  @media screen and (max-width: 400px){
+  @media screen and (max-width: 400px) {
     gap: 2rem;
   }
-`;
+`
 
 const TwitterButton = styled.a`
   img {
     height: 50px;
     widith: 50px;
   }
-`;
+`
 
 const DiscordButton = styled.a`
   img {
@@ -56,41 +64,35 @@ const DiscordButton = styled.a`
     width: 50px;
   }
 `
+
+const HeroContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
 const Logo = styled.img`
-  width: 950px;
+  max-width: 80%;
   height: auto;
   object-fit: cover;
   opacity: 90%;
-  @media screen and (max-width: 1000px){
-    width: 90%;
-  }
-`;
+`
 
 const LandingHero = styled.img`
   height: auto;
-  width: 500px;
-  margin-top: -220px;
+  max-width: 50%;
   z-index: 1;
-  @media screen and (max-width: 1000px){
-    width: 70%;
-  }
-  @media screen and (max-width: 700px){
-    margin-top: -20%;
-  }
-  @media screen and (max-width: 500px){
-    margin-top: -25%;
-  }
-
-`;
-
+  margin-top: -20%;
+`
 
 const Copy = styled.h2`
   max-width: 800px;
   text-align: center;
-  @media screen and (max-width: 500px){
-    font-size: 38px; 
-}
-`;
+  @media screen and (max-width: 500px) {
+    font-size: 38px;
+  }
+`
 
 const MintButton = styled.button`
   width: 320px;
@@ -99,7 +101,6 @@ const MintButton = styled.button`
   backround: white;
   border: none;
   margin: 1rem 0rem;
-`;
+`
 
-
-export default Landing;
+export default Landing
